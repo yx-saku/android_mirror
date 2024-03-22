6182bd1261b1c34787d3ded0c72cae64a40c0741
yx-saku <selsxprob@gmail.com>
2024-03-22 11:43:57 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 apt-get install -y ffmpeg libsdl2-2.0-0 wget \
     gcc git pkg-config meson ninja-build libsdl2-dev \
@@ -8,11 +8,11 @@ apt-get install -y ffmpeg libsdl2-2.0-0 wget \
 
 # aptでadbをインストールするとapt pairコマンドがないため、直接adb最新版をインストール
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip  && \
-    unzip platform-tools-latest-linux.zip && \
-    ln -s $(pwd)/platform-tools/adb /usr/local/bin/adb
+    unzip platform-tools-latest-linux.zip -d /adb && \
+    ln -s /adb/platform-tools/adb /usr/local/bin/adb
 
-git clone https://github.com/Genymobile/scrcpy.git && \
-    cd scrcpy && \
+git clone https://github.com/Genymobile/scrcpy.git /scrcpy && \
+    cd /scrcpy && \
     ./install_release.sh
 
 git clone https://github.com/yx-saku/adb-wifi-debug.git /adb-wifi-debug
