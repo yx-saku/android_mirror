8a2ae1fac4720d511264165e88eb752ce87b1dfd
yx-saku <selsxprob@gmail.com>
2024-03-24 02:33:28 +0900

    修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+git config --global --add safe.directory /adb-wifi-debug
+
+PWD=$(pwd)
+cd /adb-wifi-debug
+sudo git pull
+cd "$PWD"
+
 export PORT=$1
 export PAIR_PORT=$2
 export PAIR_CODE=$3
